e81d898794cb7a18ef12f523de41f69066b15aa4
Graham Sawell <graham@sawell.co.uk>
2023-10-05 18:16:15 +0100

    pwd

--- entry.sh
+++ entry.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/app/shipper --http-listen ":8081" --graphql-mode Playground --clickhouse-host=$CLICKHOUSE_HOST --clickhouse-user $CLICKHOUSE_USER --clickhouse-password $CLICKHOUSE_PASSWORD --clickhouse-debug --clickhouse-database-pattern $CLICKHOUSE_DB_PATTERN --clickhouse-table-pattern $CLICKHOUSE_TABLE_PATTERN
+/app/shipper --http-listen ":8081" --graphql-mode Playground --clickhouse-host=$CLICKHOUSE_HOST --clickhouse-user $CLICKHOUSE_USER --clickhouse-password="$CLICKHOUSE_PASSWORD" --clickhouse-debug --clickhouse-database-pattern $CLICKHOUSE_DB_PATTERN --clickhouse-table-pattern $CLICKHOUSE_TABLE_PATTERN
--- shipper.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/click-stream/shipper/cmd"
-
-func main() {
-	cmd.Execute()
-}
